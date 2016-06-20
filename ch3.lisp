70b99d57a564e651b4b0726f814b344a66652963
Jarod Tian <jarod.tian@outlook.com>
2016-06-20 11:21:01 +0800

    solution to 3.3

--- ch3.lisp
+++ ch3.lisp
@@ -11,3 +11,34 @@
       (if (member (car lst-b) lst-a)
           (append (new-union lst-a (cdr lst-b)) nil)
           (append (new-union lst-a (cdr lst-b)) (list (car lst-b))))))
+
+
+;; Solution to 3.3
+;Define a function that takes a list and returns a list indicating the
+;number of times each (eql) element appears, sorted from most common
+;element to least common:
+;> (occurrences ' ( a b a d a c d e a ) )
+;((A . 4) (C . 2) (D . 2) (B . 1))
+
+(defun occurrences (lst)
+  (sort (myoccurrences lst) #'> :key #'cdr))
+
+(defun myoccurrences (lst)
+  (if (null lst)
+      nil
+      (let ((elt (car lst)))
+        (cons (cons elt (mycount elt 0 lst)) (myoccurrences (myremove elt (cdr lst)))))))
+
+(defun mycount (elt n lst)
+  (if (null lst)
+      n
+      (if (eql elt (car lst))
+          (mycount elt (+ 1 n) (cdr lst))
+          (mycount elt n (cdr lst)))))
+
+(defun myremove (elt lst)
+  (if (null lst)
+      nil
+      (if (eql elt (car lst))
+          (myremove elt (cdr lst))
+          (cons (car lst) (myremove elt (cdr lst))))))
